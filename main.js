c309f44bfe33e9f6882cc3ac067f38eed85be271
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-03-09 15:10:18 +0200

    new update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const navbar = document.getElementById("navbar");
+const containerEl = document.querySelector(".sub-cont");
 
 // Condition to check if page is scrolled more than 100vh
 const handleScroll = () => {
@@ -10,4 +11,39 @@ const handleScroll = () => {
         }
       });
 };
-handleScroll();  
+handleScroll();
+
+const purpose = [
+   "A place of Worship",
+   "A place of Transformation",
+   "A place of Fellowship",
+   "A place of Restoration",
+   "A place of Deliverance",
+   "A place of Inspiration and Grow"
+   
+  ];
+
+  let purposeIndex = 0;
+
+  let characterIndex = 0;
+
+  updateIndex();
+
+  function updateIndex(){
+    characterIndex++;
+    containerEl.innerHTML = `
+  <span class="fs-2"> ${purpose[purposeIndex].slice(0,characterIndex)} </span>
+`;
+
+
+if(characterIndex === purpose[purposeIndex].length){
+  purposeIndex++;
+  characterIndex = 0;
+}
+
+if(purposeIndex === purpose.length){
+  purposeIndex = 0;
+}
+setTimeout(updateIndex,400);
+  }
+
